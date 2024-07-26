23058318ca4bccf7182a21363b4dcb5ed0c024da
Flouis2017 <315587951@qq.com>
2024-07-26 18:16:40 +0800

    returnNamed

--- 04_Return/Return.sol
+++ 04_Return/Return.sol
@@ -1,11 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
+// import "https://github.com/Arachnid/solidity-stringutils/blob/master/src/strings.sol";
+
 contract Return {
 
     // 返回（输出）多个变量
-    function returnMutiple() public pure returns (int, string memory, int[3] memory) {
+    function returnMutiple() external pure returns (int, string memory, int[3] memory) {
         return (1, "solidity", [int(1995), 12, 10]);
+    }
+
+    // 命名式返回
+    function returnNamed() external pure returns (int x) {
+        x = 100;
+    }
+
+    function returnNamed2() external pure returns (string memory name, int age, int gender) {
+        // string memory familyName = "Lin";
+        // string memory firstName = "Tom ";
+        name = "Tom";
+        age = 30;
+        gender = 1;
     }
 
 }
